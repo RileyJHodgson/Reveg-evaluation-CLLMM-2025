96acdce8cdb42d270c4dc0da4fd51db249ba799a
Riley J Hodgson <102647972+RileyJHodgson@users.noreply.github.com>
2026-03-25 15:47:35 +1030

    Create install_k2_db.sh

---
+++ steps-pawsey/install_k2_db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1018
+#SBATCH --job-name=download_k2_db
+#SBATCH --partition=copy
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+
+mkdir -p /scratch/pawsey1018/rhodgson/goyder/resources/k2_pluspf_20260226
+
+cd /scratch/pawsey1018/rhodgson/goyder/resources/k2_pluspf_20260226
+
+wget https://genome-idx.s3.amazonaws.com/kraken/k2_pluspf_20260226.tar.gz
+
+# extract database files
+tar -xvf k2_pluspf_20260226.tar.gz
+
+# tidy up?
+rm k2_pluspf_20260226.tar.gz
